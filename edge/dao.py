a8502ae24674f7d1d17380cb2e823f75388fbb61
Arvindh Manian <arvindh.manian@gmail.com>
2024-11-25 22:24:02 -0500

    Better 404 handling

--- edge/dao.py
+++ edge/dao.py
@@ -26,15 +26,23 @@ def request_file_from_server(app, filename, server=f"http://{os.environ['INGESTI
             cache_expiration_time = 3600  # default to 1 hour
 
         cache.set(filename, True, expire=cache_expiration_time)
+        return True
     except Exception as e:
         app.logger.error(f"Error caching file: {e}")
+        return False
 
 def fetch_file(app, filename):
     file_path = f'/app/cache/{filename}'
+    cached = False
     if not cache.get(filename):
-        request_file_from_server(app, filename)
+        cached = request_file_from_server(app, filename)
         cache_misses.inc()
     else:
         cache_hits.inc()
+        cached = True
         logging.debug(f"Fetching {filename} from cache")
-    return send_file(file_path)
+    if cached:
+        return send_file(file_path)
+    else:
+        # return a 404
+        return "File not found", 404
